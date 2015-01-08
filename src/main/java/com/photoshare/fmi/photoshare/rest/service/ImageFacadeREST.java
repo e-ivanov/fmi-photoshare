5123eb5664a0f984341b01e4af9a05f28d59ba86
Evgeni Ivanov <e.ivanov.dev@gmail.com>
2015-01-08 22:45:09 +0200

    Final version of image upload API

--- src/main/java/com/photoshare/fmi/photoshare/rest/service/ImageFacadeREST.java
+++ src/main/java/com/photoshare/fmi/photoshare/rest/service/ImageFacadeREST.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.photoshare.fmi.photoshare.rest.service;
 
 import com.photoshare.fmi.photoshare.ejb.ImageFacadeLocal;
@@ -11,6 +10,7 @@ import com.photoshare.fmi.photoshare.ejb.UserFacadeLocal;
 import com.photoshare.fmi.photoshare.entity.Image;
 import com.photoshare.fmi.photoshare.entity.User;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -35,34 +35,36 @@ import javax.ws.rs.core.Response;
 import org.apache.commons.io.IOUtils;
 import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+
 /**
  *
  * @author killer
  */
 
 @Path("image")
-public class ImageFacadeREST  {
+public class ImageFacadeREST {
+
     @PersistenceContext(unitName = "com.photoshare.fmi_photoshare_war_1.0PU")
     private EntityManager em;
-    
-    @Context HttpServletRequest request;
-    
+
+    @Context
+    HttpServletRequest request;
+
     @EJB
     private ImageFacadeLocal imageFacade;
     @EJB
     private UserFacadeLocal userFacade;
-
     
+    private final String FILE_PATH = "/home/killer/temprest/";
 
     @POST
     @Path("create")
     @Consumes("multipart/form-data")
     public Response create(MultipartFormDataInput form) {
-        //super.create(entity);
-        String FILE_PATH = "/home/killer/temprest/";
+        
         String filename = "";
         Map<String, List<InputPart>> uploadForm = form.getFormDataMap();
-        
+        String FINAL_PATH = "";
         String fileName;
         String username;
         String description;
@@ -74,9 +76,9 @@ public class ImageFacadeREST  {
             Image entity = new Image();
             entity.setDescription(description);
             entity.setUserId(userFacade.findUserByUserName(username));
-            entity.setFileName(FILE_PATH+fileName);
             
-            imageFacade.create(entity);
+            
+            
             
             List<InputPart> inputParts = uploadForm.get("fileUpload");
             
@@ -86,39 +88,47 @@ public class ImageFacadeREST  {
             {
                 @SuppressWarnings("unused")
                 MultivaluedMap<String, String> headerParts = inputPart.getHeaders();
-                 
+                fileName = getOriginalFileName(headerParts);
                 InputStream inputStream = inputPart.getBody(InputStream.class, null);
                  
                 byte[] bytes = IOUtils.toByteArray(inputStream);
                 // constructs upload file path
-                fileName = FILE_PATH + fileName;
-                writeFile(bytes, fileName);
+                FINAL_PATH = FILE_PATH+fileName;
+                writeFile(bytes, FINAL_PATH);
             }
             catch (Exception e)
             {
                 e.printStackTrace();
             }
         }
+             entity.setFileName(fileName);
+             imageFacade.create(entity);
         return Response.status(200).entity("Uploaded file name : "+ fileName).build();
         } catch (IOException ex) {
             Logger.getLogger(ImageFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
-            return Response.status(200).entity("Failuer").build();
+            return Response.status(409).entity("An error has occured while uploading file").build();
         }
      
-        
+
     }
-    
 
     
     @GET
+    @Produces("image/*")
+    @Path("getfile/{filepath}")
+    public InputStream getFileInputStream(@PathParam("filepath") String filename)throws Exception{
+    FileInputStream fileIs = new FileInputStream(FILE_PATH+filename);
+    return fileIs;
+  }
+    
+    @GET
     @Path("test")
-    public String getstr(){
-        
+    public String getstr() {
+
         return "pingg";
     }
-    
-    private void writeFile(byte[] content, String filename) throws IOException
-    {
+
+    private void writeFile(byte[] content, String filename) throws IOException {
         File file = new File(filename);
         if (!file.exists()) {
             file.createNewFile();
@@ -129,15 +139,12 @@ public class ImageFacadeREST  {
         fop.close();
     }
 
-   
-
     @GET
     @Produces({"application/json"})
     public List<Image> findAll() {
         //return super.findAll();
         return imageFacade.findAll();
     }
-
 
     @GET
     @Path("count")
@@ -147,6 +154,20 @@ public class ImageFacadeREST  {
         return String.valueOf(imageFacade.count());
     }
 
+    private String getOriginalFileName(MultivaluedMap<String, String> headers) {
 
-    
+        String[] contentDispositionHeader = headers.getFirst("Content-Disposition").split(";");
+
+        for (String name : contentDispositionHeader) {
+
+            if ((name.trim().startsWith("filename"))) {
+                String[] tmp = name.split("=");
+                String fileName = tmp[1].trim().replaceAll("\"", "");
+                return fileName;
+            }
+        }
+        return "randomName";
+
+    }
+
 }
